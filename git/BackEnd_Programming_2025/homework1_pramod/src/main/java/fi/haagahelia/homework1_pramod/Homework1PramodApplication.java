8ad48e39b3f2fb06a48817b269d886a7d587ce01
Pramod <bappramod@gmail.com>
2025-01-23 08:37:25 +0200

    all files

---
+++ git/BackEnd_Programming_2025/homework1_pramod/src/main/java/fi/haagahelia/homework1_pramod/Homework1PramodApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.homework1_pramod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework1PramodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework1PramodApplication.class, args);
+	}
+
+}
---
+++ git/BackEnd_Programming_2025/homework1_pramod/src/test/java/fi/haagahelia/homework1_pramod/Homework1PramodApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.homework1_pramod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework1PramodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
